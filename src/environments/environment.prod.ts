7c141036a48f3e0755ecf1c0a193ebfbfcef7f81
Unknown <martez_conner@outlook.com>
2018-01-27 02:24:32 -0800

    Initial commit and project almost finish for completion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyChoWT8zz3lH3PKibPlTVpnsCx3uhwi4Do",
+    authDomain: "clientpanelprod-a3e6b.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-a3e6b.firebaseio.com",
+    projectId: "clientpanelprod-a3e6b",
+    storageBucket: "clientpanelprod-a3e6b.appspot.com",
+    messagingSenderId: "623080729389"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyChoWT8zz3lH3PKibPlTVpnsCx3uhwi4Do",
+    authDomain: "clientpanelprod-a3e6b.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-a3e6b.firebaseio.com",
+    projectId: "clientpanelprod-a3e6b",
+    storageBucket: "clientpanelprod-a3e6b.appspot.com",
+    messagingSenderId: "623080729389"
+  }
 };
